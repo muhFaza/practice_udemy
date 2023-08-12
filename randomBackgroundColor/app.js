d3f7462133140489811bc3ee43a260ee6dd1ded4
muhFaza <fazamuhammad30@gmail.com>
2023-08-13 02:59:24 +0700

    added tooDark function, if background is too dark h1 color will change to whtie

--- randomBackgroundColor/app.js
+++ randomBackgroundColor/app.js
@@ -3,16 +3,16 @@ let button = document.querySelector('button')
 
 button.addEventListener('click', () =>{
     document.body.style.background = `rgb(${randColor()}, ${randColor()}, ${randColor()})`
-    h1.innerText = document.body.style.background 
+    h1.innerText = `${document.body.style.background} ${tooDark(document.body.style.background)}`
+    tooDark(document.body.style.background) > 250 ? h1.style.color = 'black' : h1.style.color = 'white'
     button.innerText = `YEAYYYYYYY`
 })
 
-function tooDark (){
-    let rgb = document.body.style.background
-    rgb.slice(4, rgb.length-1).split(', ')
-    let sum = rgb.reduce( (x, 0) => x )
-
+function tooDark (rgb, num = 0){
+    rgb = rgb.slice(4, rgb.length-1).split(', ').forEach(x => num += parseInt(x));
+    return num
 }
+// tooDark('rgb(244, 54, 154)')
 
 function randColor (){
     return Math.floor(Math.random() * 255)
